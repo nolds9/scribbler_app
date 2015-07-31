892596db5dfa28236b7881597925e533a7fd24e4
Nicholas Olds <nolds9@yahoo.com>
2015-07-31 12:59:55 -0400

    Scaling down comments, and fixing username params for devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730005225) do
+ActiveRecord::Schema.define(version: 20150731162552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
-    t.string   "title"
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at", null: false
